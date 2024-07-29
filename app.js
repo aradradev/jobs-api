28689d23db0b1edcd59cf351c792cfa06ed3cd5c
aradradev <abdoulramanediallo44@gmail.com>
2024-07-29 18:20:30 +0000

    add swagger ui docs

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger UI
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -30,7 +35,8 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-app.get('/', (req, res) => res.send('JOBS-API'))
+app.get('/', (req, res) => res.send('<h1>JOBS-API</h1><a href="/api-docs">Documentation</a>'))
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1', authenticateUser, jobsRouter)
--- db/connect.js
+++ db/connect.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = (url) => {
   return mongoose
-    .connect(url, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    })
+    .connect(url)
     .then(console.log('MongoDB Connected...'))
     .catch((err) => console.log(`Something went wrong with the server ${err}`))
 }
